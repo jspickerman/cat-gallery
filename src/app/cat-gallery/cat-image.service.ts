84c7dd0627080db9d473f40fd2a2c2b11311784c
jspickerman <jonathanspickerman@gmail.com>
2020-12-21 13:44:34 -0600

    1 file modified

--- src/app/cat-gallery/cat-image.service.ts
+++ src/app/cat-gallery/cat-image.service.ts
@@ -1,8 +1,28 @@
+import { HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+
+export interface CatImage {
+  breeds: string[];
+  id: string;
+  url: string;
+  width: string;
+  height: string;
+}
 
 @Injectable()
 export class CatImageService {
 
+  IMAGE_ENDPOINT: string = 'api.thecatapi.com/v1/images/search';
+  DEFAULT_IMAGE_TYPE: string = 'image';
+  DEFAULT_LIMIT: string = "1";
+
   constructor() { }
+
+  public getImages(imageType?: string, limit?: number): Observable<CatImage[]> {
+    const params = new HttpParams().set('mime_types', imageType ? imageType : this.DEFAULT_IMAGE_TYPE)
+    .set('limit', limit ? limit : this.DEFAULT_LIMIT);
+
+  }
 
 }
