386e540bdb05e82da2999b1c8408ea552d3329aa
jspickerman <jonathanspickerman@gmail.com>
2020-12-21 13:31:51 -0600

    1 file modified, 4 added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,10 +5,13 @@ import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
 import { CatGalleryComponent } from './cat-gallery/cat-gallery.component';
+import { CatGalleryItemComponent } from './cat-gallery/cat-gallery-item/cat-gallery-item.component';
+import { CatImageServiceService } from './cat-gallery/cat-image-service.service';
 
 @NgModule({
   imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, HelloComponent, CatGalleryComponent ],
-  bootstrap:    [ AppComponent ]
+  declarations: [ AppComponent, HelloComponent, CatGalleryComponent, CatGalleryItemComponent ],
+  bootstrap:    [ AppComponent ],
+  providers: [CatImageServiceService]
 })
 export class AppModule { }
---
+++ src/app/cat-gallery/cat-gallery-item/cat-gallery-item.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-cat-gallery-item',
+  templateUrl: './cat-gallery-item.component.html',
+  styleUrls: ['./cat-gallery-item.component.css']
+})
+export class CatGalleryItemComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
