c330219f8d4bc0f861f0599992c9fbd2d11401b3
jspickerman <jonathanspickerman@gmail.com>
2020-12-28 14:28:36 -0600

    2 files modified

--- src/app/state/cat-gallery.actions.ts
+++ src/app/state/cat-gallery.actions.ts
@@ -4,22 +4,22 @@ import { CatImageData} from "./cat-gallery.state";
 export const GetImages = createAction(
   '[Cat Gallery] Get Images',
   props<{imageType: string, limit: string}>()
-)
+);
 
 export const ImagesLoaded = createAction(
   '[Cat Gallery] Images Loaded',
   props<{imageResponse: CatImageData[]}>()
-)
+);
 
 export const FilterGIF = createAction( 
   '[Cat Gallery] Filter to GIfs'
-)
+);
 
 export const FilterJPG = createAction(
   '[Cat Gallery] Filter to JPGs'
-)
+);
 
 export const FilterPNG = createAction(
   '[Cat Gallery] Filter to PNGs'
-)
+);
 
