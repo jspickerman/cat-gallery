e319210f6fee795edda174e6549c7201ba315323
jspickerman <jonathanspickerman@gmail.com>
2021-01-04 15:20:25 -0600

    1 file modified

--- src/app/state/cat-gallery.selectors.ts
+++ src/app/state/cat-gallery.selectors.ts
@@ -1,14 +1,7 @@
 import { createFeatureSelector, createSelector } from "@ngrx/store";
-import { CatGalleryState, IMAGE_TYPE } from "./cat-gallery.state";
+import { CatGalleryState } from "./cat-gallery.state";
 
-export const selectCatGalleryState = createFeatureSelector<CatGalleryState>('catgallery');
-
-export const images = createSelector(
-  selectCatGalleryState,
-  (state: CatGalleryState) => {
-    return state.imageData;
-  }
-)
+export const selectCatGalleryState = createFeatureSelector<CatGalleryState>('catgallery')
 
 export const filters = createSelector(
   selectCatGalleryState,
