32bd9d368b327077aa5c9fff0badd911135ac305
jspickerman <jonathanspickerman@gmail.com>
2021-01-04 10:16:05 -0600

    1 file modified

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -12,6 +12,7 @@ import { EffectsModule } from '@ngrx/effects';
 import { catGalleryReducers } from './state/cat-gallery.reducers';
 import {CatGalleryEffects} from './state/cat-gallery.effects';
 import { HttpClientModule } from '@angular/common/http';
+import { CatGalleryFilterComponent } from './cat-gallery/cat-gallery-filter/cat-gallery-filter.component';
 
 @NgModule({
   imports: [BrowserModule, FormsModule, HttpClientModule, StoreModule.forRoot({'catgallery': catGalleryReducers}), EffectsModule.forRoot([CatGalleryEffects])],
