7fe90304073172b0afa909be722ae1097cb6f4ce
jspickerman <jonathanspickerman@gmail.com>
2021-01-04 11:39:56 -0600

    3 files modified

--- src/app/cat-gallery/cat-gallery.component.ts
+++ src/app/cat-gallery/cat-gallery.component.ts
@@ -2,7 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { Store } from '@ngrx/store';
 import { CatGalleryState } from '../state/cat-gallery.state';
 import * as CatGalleryActions from '../state/cat-gallery.actions'
-import { filteredImages, filters, images } from '../state/cat-gallery.selectors';
+import { filteredImages, filters } from '../state/cat-gallery.selectors';
 
 @Component({
   selector: 'app-cat-gallery',
