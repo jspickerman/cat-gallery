942b40d69710fea794fb94a03d56193cc29df565
jspickerman <jonathanspickerman@gmail.com>
2021-01-12 13:57:53 -0600

    1 file modified

--- src/app/cat-gallery/cat-gallery.component.ts
+++ src/app/cat-gallery/cat-gallery.component.ts
@@ -2,7 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { select, Store } from '@ngrx/store';
 import { CatGalleryState } from '../state/cat-gallery.state';
 import * as CatGalleryActions from '../state/cat-gallery.actions'
-import { filteredImages, selectFilters } from '../state/cat-gallery.selectors';
+import { selectFilters, selectImageData } from '../state/cat-gallery.selectors';
 
 @Component({
   selector: 'app-cat-gallery',
@@ -13,8 +13,8 @@ export class CatGalleryComponent implements OnInit {
 
   DEFAULT_LIMIT: string = "25";
 
-  images$ = this.store.pipe(select(filteredImages));
-  filters$ = this.store.select(selectFilters);
+  images$ = this.store.pipe(select(selectImageData));
+  filters$ = this.store.pipe(select(selectFilters));
 
   constructor(private store: Store<CatGalleryState>) { }
 
