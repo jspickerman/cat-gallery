05f573520fff172e579c8c956ceb0d7a462b11a4
jspickerman <jonathanspickerman@gmail.com>
2021-01-13 14:46:19 -0600

    1 file modified

--- src/app/cat-gallery/cat-gallery.component.ts
+++ src/app/cat-gallery/cat-gallery.component.ts
@@ -14,7 +14,14 @@ export class CatGalleryComponent implements OnInit {
 
   DEFAULT_LIMIT: number = 25;
 
-  images$ = this.store.pipe(select(selectFilteredImages));
+  images$ = this.store.pipe(select(selectFilteredImages),
+    tap((imageData: CatImageData) => {
+      console.log(imageData);
+      if (imageData.images.length < this.DEFAULT_LIMIT) {
+        console.log('under!');
+      }
+    })
+  );
   filters$ = this.store.pipe(select(selectFilters));
 
   constructor(private store: Store<CatGalleryState>) { }
