8b0f9c78103b8075c171b93686ff049bd875f2b1
jspickerman <jonathanspickerman@gmail.com>
2021-01-13 15:16:53 -0600

    1 file modified

--- src/app/cat-gallery/cat-image.service.ts
+++ src/app/cat-gallery/cat-image.service.ts
@@ -1,6 +1,7 @@
 import { HttpClient, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
+import { of } from 'rxjs/operators';
 import { CatImageData } from '../state/cat-gallery.state';
 
 @Injectable()
